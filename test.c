2dfd28039aa049bb32dd6a355a43f9db62d1ade3
Saulius Menkevičius <saulius@forallsecure.com>
2023-01-03 11:29:20 +0200

    test.c: sprinkle in fn points in between the buffer to be overridden

--- test.c
+++ test.c
@@ -6,13 +6,19 @@ void fn() {
 
 int main(int argc, char** argv)
 {
-    char buffer[10];
     void (*pfn0)(void);
-    int buffer_i = 0;
+    char buffer[10];
     void (*pfn1)(void);
+    int buffer_i = 0;
+    void (*pfn2)(void);
 
     pfn0 = fn;
     pfn1 = fn;
+    pfn2 = fn;
+
+    pfn0();
+    pfn1();
+    pfn2();
 
     while (1) {
         int c = getchar();
@@ -27,8 +33,14 @@ int main(int argc, char** argv)
     buffer[buffer_i] = 0; // add terminator
 
     pfn0();
-    printf("buffer=%s\n", buffer);
     pfn1();
+    pfn2();
+
+    printf("buffer=%s\n", buffer);
+
+    pfn0();
+    pfn1();
+    pfn2();
 
     return 0;
 }
